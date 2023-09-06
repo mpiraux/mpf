d8ead2be27b6a9795b36f8349ef39a55696da656
Maxime Piraux <maxime.piraux@uclouvain.be>
2023-09-06 09:07:14 +0200

    Adds a minimal setup.py for editable installs

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mpf',
+    version='0.0.1',
+    url='https://github.com/mpiraux/mpf.git',
+    author='Maxime Piraux',
+    author_email='maxime.piraux@uclouvain.be',
+    description='Minimal Performance Framework',
+    packages=find_packages(),    
+    install_requires=[
+        'ipyparallel >= 8.6', 
+        'pandas >= 1.5',
+        'PyYAML >= 6.0'
+        ],
+)
